85ed55713455884a39dd2cf80b0549634e6d24a4
Benjamin Potestas <benjaminpotestas@csu.fullerton.edu>
2023-10-30 14:15:38 -0700

    made a code that find the mean of multiple numbers.  Passed test & formatting.  Has Lint errors.

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,31 +1,31 @@
-// TODO: Add the required header
+// Sean Young
+// younggseann16@csu.fullerton.edu
+// @1SeanYoung
+// Partners: @print-benjamin
 
 #include <iostream>
 #include <string>
 #include <vector>
+using namespace std;
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> arguments{argv, argv + argc};
+  vector<string> arguments{argv, argv + argc};
 
-  // TODO: Validate that there is at least one command line argument.
-  // If not, print an error message and return a non-zero value.
+  if (arguments.size() <= 1) {
+    cout << "error: you must supply at least one number \n";
+    return 1;
+  }
 
-  // TODO: Write a for-each loop to sum (add up) all of the command line
-  // arguments.
-  // Use a double or float type so that your program preserves fractional
-  // values.
-  // The loop needs to skip over the command name, which is the first element
-  // of the arguments vector.
-  // Each argument is a std::string. You will need to convert each string into
-  // a number with the std::stod or std::stof function.
+  double sum{0.0};
 
-  // TODO: After the loop has finished summing the arguments, calculate the
-  // average of the values. Recall that the average is the total value divided
-  // by the number of values.
+  for (int i = 1; i < arguments.size(); i++) {
+    sum += stod(arguments[i]);
+  }
 
-  // TODO: Use cout to print out a message of the form
-  // average = *AVERAGE*
-  // on its own line.
+  double average =
+      static_cast<double>(sum) / static_cast<double>(arguments.size() - 1);
+
+  cout << "average = " << average << "\n";
 
   return 0;
 }
